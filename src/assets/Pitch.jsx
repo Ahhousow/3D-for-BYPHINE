0f074319ea7b08d16de8ae0c1096345f24e6ffb7
Ahhousow <biabianyvincent@gmail.com>
2025-03-18 09:43:51 +0100

    Css migration

--- src/assets/Pitch.jsx
+++ src/assets/Pitch.jsx
@@ -5,15 +5,15 @@ function Pitch({ model }) {
     <div className="pitch"> 
        <div className="bloc---detail">
          <div className="content-fragment">
-                {Array.isArray(model) ? model : <div id="p-1" dangerouslySetInnerHTML={{ __html: model.p1 }} />}
-                {Array.isArray(model) ? model : <div id="p-2" dangerouslySetInnerHTML={{ __html: model.p2 }} />}
+                {Array.isArray(model) ? model : <div id="p-1" className="content-p" dangerouslySetInnerHTML={{ __html: model.p1 }} />}
+                {Array.isArray(model) ? model : <div id="p-2" className="content-p" dangerouslySetInnerHTML={{ __html: model.p2 }} />}
                 <div className="reach-us">
                        <span className="content-p">have a needs ?</span>
                        <img className="vectors-wrapper-5" alt="" loading="lazy" />
                        <span className="content-p">reach us</span>
                 </div> 
                 <div className="vector-path"></div>  
-                {Array.isArray(model) ? model : <div id="p-3" dangerouslySetInnerHTML={{ __html: model.p3}} />}     
+                {Array.isArray(model) ? model : <div id="p-3" className="content-p" dangerouslySetInnerHTML={{ __html: model.p3}} />}     
                 <a href="#" target="_blank" className="button-2"> DISCOVER</a>
          </div>
        </div>
