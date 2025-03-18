e93fa4d35e69745e37889af24e467b8dbdf1b8d6
Ahhousow <biabianyvincent@gmail.com>
2025-03-18 20:43:55 +0100

    Responsive shit

--- src/assets/Camera.jsx
+++ src/assets/Camera.jsx
@@ -1,7 +1,27 @@
-import React, { forwardRef } from 'react';
-import { PerspectiveCamera } from '@react-three/drei'
+import React, { forwardRef, useState, useEffect } from 'react';
+import { PerspectiveCamera } from '@react-three/drei';
 
 const Camera = forwardRef((props, ref) => {
+  const [cameraZ, setCameraZ] = useState(350);
+
+  useEffect(() => {
+    // Function to update cameraZ based on window width
+    const handleResize = () => {
+      if (window.innerWidth < 768) {
+        setCameraZ(750);
+      } else {
+        setCameraZ(350);
+      }
+    };
+
+    // Check on mount
+    handleResize();
+
+    // Add event listener for window resize
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <PerspectiveCamera
       ref={ref}
@@ -10,7 +30,7 @@ const Camera = forwardRef((props, ref) => {
       far={1000}
       near={0.1}
       fov={27}
-      position={[0, 0, 350]}
+      position={[0, 0, cameraZ]}
     />
   );
 });
