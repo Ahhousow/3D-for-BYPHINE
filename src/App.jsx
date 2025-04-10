5fb1c68b0ccde08de4a21f32766491f02a5a0aeb
Ahhousow <biabianyvincent@gmail.com>
2025-04-10 13:50:40 +0200

    Test Responsive 6

--- src/App.jsx
+++ src/App.jsx
@@ -56,16 +56,16 @@ export default function App() {
 
   ];
   const responsivePositions = basePositions.map(([x, y, z]) => {
-  //  const responsiveX = isMobile ? x / 1.2 : x;
+  const responsiveX = isMobile ? x / 1.2 : x;
     let responsiveY = y;
-    let responsiveX = x;
+  //  let responsiveX = x;
 
     if (isMobile) {
       if (y === 60) responsiveY = 50;
       else if (y === -60) responsiveY = -50;
 
-      if (x === 130) responsiveX = 80;
-      else if (x === -130) responsiveX = -80;
+      // if (x === 130) responsiveX = 80;
+      // else if (x === -130) responsiveX = -80;
     }
     return [responsiveX, responsiveY, z];
   });
